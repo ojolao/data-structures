0dce6216a200ccfc7bacd0683b755127f4b49b1d
ojolao <jolaosho@Omotayos-MacBook-Pro.local>
2017-02-11 14:39:05 -0800

    pass all tests for graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -10,8 +10,6 @@ var Graph = function() {
 Graph.prototype.addNode = function(node) {
   this.nodes.push(node);
   this.edges[node] = [];
-  console.log(this.nodes);
-	
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
@@ -29,17 +27,22 @@ Graph.prototype.removeNode = function(node) {
   if (this.nodes.indexOf(node) !== -1) {
     this.nodes.splice(this.nodes.indexOf(node), 1);
   }
+  if (this.edges[node].length > 0) {
+    this.edges[node].length = 0;
+  }
+  this.edges.forEach(function(edge) {
+    if (edge.indexOf(node) !== -1) {
+      edge.splice(edge.indexOf(node), 1);
+    }
+  });
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
   var search = false;
-  for (var i = 0; i < this.edges[fromNode].length; i++) {
-    if (this.edges[fromNode][i] === toNode && this.edges[toNode][i] === fromNode) {
-      search = true;
-    }
+  if (this.edges[fromNode].indexOf(toNode) !== -1 && this.edges[toNode].indexOf(fromNode) !== -1) {
+    search = true;
   }
-  console.log(this.edges[fromNode], this.edges[toNode]);
   return search;
 };
 
@@ -52,10 +55,17 @@ Graph.prototype.addEdge = function(fromNode, toNode) {
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  if (this.edges[fromNode].indexOf(toNode) !== -1 && this.edges[toNode].indexOf(fromNode) !== -1) {
+    this.edges[fromNode].splice(this.edges[fromNode].indexOf(toNode), 1);
+    this.edges[toNode].splice(this.edges[toNode].indexOf(fromNode), 1);
+  }
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  this.nodes.forEach(function(node) {
+    cb(node);
+  });
 };
 
 /*

0061f878a7e6b5ad20fc6f7a85e4f5e5e7346551
ojolao <jolaosho@Omotayos-MacBook-Pro.local>
2017-02-11 15:05:05 -0800

    implement all methods in set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,30 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  result = false;
+  this._storage.forEach(function(element) {
+    (item === element) ? result = true : result;
+  });
+  return result;
 };
 
 setPrototype.remove = function(item) {
+  currentSet = this._storage;
+  currentSet.forEach(function(element) {
+    if (item === element) { 
+      currentSet.splice(currentSet.indexOf(element), 1);
+    }
+  });
 };
 
 /*

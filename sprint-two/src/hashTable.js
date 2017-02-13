2db57b49f797846f0b5e3ce01c7e4adbf22fe0bb
ojolao <jolaosho@Omotayos-MBP.attlocal.net>
2017-02-12 17:55:38 -0800

    hash table insert, retrieve and remove implemented and pass all texts except advanced

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,42 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage[index] === undefined) {
+    this._storage[index] = [[k, v]];   
+  } else {
+    var keyAlreadyExists = false;
+    for (var i = 0; i < this._storage[index].length; i++) {
+      if (this._storage[index][i][0] === k) {
+        this._storage[index][i][1] = v;
+        keyAlreadyExists = true;
+      } 
+    }
+    if (keyAlreadyExists === false) {
+      this._storage[index].push([k, v]);
+    }
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage[index];
+  var result;
+  console.log(bucket);
+  bucket.forEach(function(tuple) {
+    if (tuple[0] === k) {
+      result = tuple[1];
+    }
+  });
+  return result;
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = getIndexBelowMaxForKey(k, this._limit); 
+  for (var i = 0; i < this._storage[index].length; i++) {
+    if (this._storage[index][i][0] === k) {
+      this._storage[index].splice(this._storage[index].indexOf(i), 1);
+    }
+  }
 };
 
 
